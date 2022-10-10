6b56c7b8a838a2ea8f5292f04d0eb631f055d8d3
Vsevolod Golovko <VsGolovko@luxoft.com>
2022-10-10 18:30:04 +0200

    Device Selection partly done

---
+++ RprUsd/src/python/deviceConfigRunner.py
@@ -0,0 +1,14 @@
+import shiboken2
+from PySide2 import QtCore
+from PySide2 import QtWidgets
+import maya.OpenMayaUI as apiUI
+
+from rpr import RprUsd
+
+def getMayaMainWindow():
+    ptr = apiUI.MQtUtil.mainWindow()
+    if ptr is not None:
+        return shiboken2.wrapInstance(int(ptr), QtWidgets.QWidget)
+
+def open_window() :
+    RprUsd.devicesConfiguration.open_window(getMayaMainWindow(), QtCore.Qt.Window)
