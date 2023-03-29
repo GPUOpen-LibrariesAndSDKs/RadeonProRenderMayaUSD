f3c6f5ff7f7346a4376bfa548cb4d56d44da2a88
Vsevolod Golovko <VsGolovko@luxoft.com>
2023-03-29 14:04:37 +0200

    some changes for 2024 adaptation

--- build_with_devkit_2024.bat
+++ build_with_devkit_2024.bat
@@ -14,7 +14,7 @@ echo Maya_sdk=%Maya_sdk%
 echo Python_Include_Dir=%Python_Include_Dir%
 echo Python_Library=%Python_Library%
 
-set usd_build_fullpath=%MAYA_x64_2024%\..\MayaUSD\Maya2024\0.22.0_202301152314-a488582\mayausd\USD
+set usd_build_fullpath=%MAYA_x64_2024%\..\MayaUSD\Maya2024\0.22.0\mayausd\USD
 
 echo Building RadeonProRenderUSD (hdRPR) within builtin Maya's USD package...
 
